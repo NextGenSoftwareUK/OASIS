8acc004fd72c3eacc56777dc2cef801180618c4d
David Ellams <davidellams@hotmail.com>
2025-09-04 20:19:31 +0000

    - Continued improving the STARNET Custom Tag system and fixing bugs.
    - They are now contained within a new MetaTagMappings constainer which in itself contains MetaHolonTag's.
    
    - Updated HoloDNATemplate to support singular pattern instances of generated holons making custom tags easier to use and more powerful.

--- NextGenSoftware.OASIS.API.Core/Interfaces/STAR/STARNET System/ISTARNETDNA .cs
+++ NextGenSoftware.OASIS.API.Core/Interfaces/STAR/STARNET System/ISTARNETDNA .cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Newtonsoft.Json;
-using NextGenSoftware.OASIS.API.Core.Objects;
 using NextGenSoftware.OASIS.API.ONODE.Core.Objects.STARNET;
 
 namespace NextGenSoftware.OASIS.API.Core.Interfaces.STAR
@@ -21,8 +19,9 @@ namespace NextGenSoftware.OASIS.API.Core.Interfaces.STAR
         public Dictionary<string, object> MetaData { get; set; }
         //public Dictionary<string, (string, string)> MetaHolonTagMappings { get; set; }
         //[JsonObjectAttribute]
-        public List<MetaHolonTag> MetaHolonTagMappings { get; set; }
-        public Dictionary<string, string> MetaTagMappings { get; set; }
+        //public List<MetaHolonTag> MetaHolonTagMappings { get; set; }
+        //public Dictionary<string, string> MetaTagMappings { get; set; }
+        public MetaTagMappings MetaTagMappings { get; set; }
         string SourcePath { get; set; }
         string PublishedPath { get; set; }
         Guid PublishedByAvatarId { get; set; }
