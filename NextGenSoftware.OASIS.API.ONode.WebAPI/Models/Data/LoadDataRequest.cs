16008e4c5f33ce4e0a7b781c4abff1a2407736bd
David Ellams <davidellams@hotmail.com>
2025-09-06 21:47:24 +0000

    Fixed some merge issues and updated postman json import file to latest OASIS API v4.0.0.

--- NextGenSoftware.OASIS.API.ONode.WebAPI/Models/Data/LoadDataRequest.cs
+++ NextGenSoftware.OASIS.API.ONode.WebAPI/Models/Data/LoadDataRequest.cs
@@ -1,9 +1,12 @@
-
+using System;
+using NextGenSoftware.OASIS.API.Core.Holons;
 
 namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Models.Data
 {
     public class LoadDataRequest : OASISRequest
     {
         public string Key { get; set; }
+        public string Provider { get; set; }
+        public Guid? AvatarId { get; set; }
     }
 }
--- NextGenSoftware.OASIS.API.ONode.WebAPI/Models/Data/LoadFileRequest.cs
+++ NextGenSoftware.OASIS.API.ONode.WebAPI/Models/Data/LoadFileRequest.cs
@@ -1,10 +1,12 @@
-
 using System;
+using NextGenSoftware.OASIS.API.Core.Holons;
 
 namespace NextGenSoftware.OASIS.API.ONODE.WebAPI.Models.Data
 {
     public class LoadFileRequest : OASISRequest
     {
         public Guid Id { get; set; }
+        public string Provider { get; set; }
+        public Guid? AvatarId { get; set; }
     }
 }
