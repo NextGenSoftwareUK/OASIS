fe5ffa023be4388dcc8667f7757c4fe03b093d21
Jade Zaher <contact@ahmedzaher.net>
2025-09-06 22:46:39 -0600

    remove unused projects files and so on merge latest.

--- NextGenSoftware.OASIS.API.Core/Objects/NFT/Requests/MintNFTTransactionRequest.cs
+++ NextGenSoftware.OASIS.API.Core/Objects/NFT/Requests/MintNFTTransactionRequest.cs
@@ -28,6 +28,6 @@ namespace NextGenSoftware.OASIS.API.Core.Objects.NFT.Request
         public NFTStandardType NFTStandardType { get; set; }
         public NFTOffChainMetaType NFTOffChainMetaType { get; set; }
         public string Symbol { get; set; }
-        public string JSONMetaDataUrl { get; set; }
+        public string JSONMetaDataURL { get; set; }
     }
 }
