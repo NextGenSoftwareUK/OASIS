454d87cf2b8379bc5074a7634b984e2be4453892
David Ellams <davidellams@hotmail.com>
2025-11-07 22:41:43 +0000

    Fixed many bugs in the new NFT API (v2)! :)

---
+++ STAR ODK/NextGenSoftware.OASIS.STAR.CLI.Lib/Objects/SalesInfo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace NextGenSoftware.OASIS.STAR.CLI.Lib.Objects
+{
+    public class SalesInfo
+    {
+        public bool? IsForSale { get; set; }
+        public DateTime? SaleStartDate { get; set; }
+        public DateTime? SaleEndDate { get; set; }
+    }
+}
