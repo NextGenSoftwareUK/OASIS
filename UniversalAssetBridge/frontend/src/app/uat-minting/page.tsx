44558a99d90fdd4b0009879afa5a65cad934805b
Max Gershfield <max.gershfield1@gmail.com>
2025-11-14 20:22:17 +0000

    uab-frontend: add uat builder & minting flows

---
+++ UniversalAssetBridge/frontend/src/app/uat-minting/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const UatWorkspace = dynamic(
+  () =>
+    import("../../../../oasis-oracle-frontend/src/components/uat/uat-workspace").then(
+      (mod) => mod.UatWorkspace
+    ),
+  { ssr: false }
+);
+
+export default function UatMintingPage() {
+  return <UatWorkspace />;
+}
+
+
