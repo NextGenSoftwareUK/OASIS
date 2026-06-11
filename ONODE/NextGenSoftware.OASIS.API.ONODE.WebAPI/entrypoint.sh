a8c5fa119d16d4881de293a702413093b0b160b6
David Ellams <davidellams@hotmail.com>
2026-06-10 22:04:21 -0500

    Update entrypoint.sh

--- ONODE/NextGenSoftware.OASIS.API.ONODE.WebAPI/entrypoint.sh
+++ ONODE/NextGenSoftware.OASIS.API.ONODE.WebAPI/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-# Write OASIS_DNA.json from env var if provided (set in Railway Variables)
 if [ -n "$OASIS_DNA_JSON" ]; then
-    echo "$OASIS_DNA_JSON" > /app/OASIS_DNA.json
+    printf '%s' "$OASIS_DNA_JSON" > /app/OASIS_DNA.json
+    echo "OASIS_DNA.json written ($(wc -c < /app/OASIS_DNA.json) bytes)"
+else
+    echo "WARNING: OASIS_DNA_JSON not set - OASIS_DNA.json will not be written"
 fi
 exec dotnet NextGenSoftware.OASIS.API.ONODE.WebAPI.dll
