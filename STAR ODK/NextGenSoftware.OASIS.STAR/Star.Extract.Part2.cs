14e9ac667724caca00c1f829917925e0fdab510c
David Ellams <davidellams@hotmail.com>
2026-08-10 22:54:35 -0500

    refactor: split Star.Extract.cs (1011L) into 3 partial class files

---
+++ STAR ODK/NextGenSoftware.OASIS.STAR/Star.Extract.Part2.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NextGenSoftware.Utilities;
+using NextGenSoftware.Utilities.ExtentionMethods;
+using NextGenSoftware.OASIS.Common;
+using NextGenSoftware.OASIS.API.DNA;
+using NextGenSoftware.OASIS.API.Core.Enums;
+using NextGenSoftware.OASIS.API.Core.Events;
+using NextGenSoftware.OASIS.API.Core.Holons;
+using NextGenSoftware.OASIS.API.Core.Helpers;
+using NextGenSoftware.OASIS.API.Core.Objects;
+using NextGenSoftware.OASIS.API.Core.Managers;
+using NextGenSoftware.OASIS.API.Core.Interfaces;
+using NextGenSoftware.OASIS.API.Native.EndPoint;
+using NextGenSoftware.OASIS.API.Core.Interfaces.STAR;
+using NextGenSoftware.OASIS.STAR.DNA;
+using NextGenSoftware.OASIS.STAR.Enums;
+using NextGenSoftware.OASIS.STAR.Zomes;
+using NextGenSoftware.OASIS.STAR.EventArgs;
+using NextGenSoftware.OASIS.STAR.ErrorEventArgs;
+using NextGenSoftware.OASIS.STAR.CelestialSpace;
+using NextGenSoftware.OASIS.STAR.CelestialBodies;
+using NextGenSoftware.OASIS.API.ONODE.Core.Holons;
+using static NextGenSoftware.OASIS.API.Core.Events.EventDelegates;
+
+using NextGenSoftware.OASIS.STAR.Interfaces;
+using SevenZip.Buffer;
+using System.Linq;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+
+namespace NextGenSoftware.OASIS.STAR
+{
+    public static partial class STAR
+    {
+
+
+        // Build
+        public static CoronalEjection Flare(string bodyName)
+        {
+            //TODO: Build rust code using hc conductor and .net code using dotnet compiler.
+            return new CoronalEjection();
+        }
+
+        public static CoronalEjection Flare(ICelestialBody body)
+        {
+            //TODO: Build rust code using hc conductor and .net code using dotnet compiler.
+            return new CoronalEjection();
+        }
+
+        //Activate & Launch - Launch & activate a planet (OApp) by shining the star's light upon it...
+        public static void Shine(ICelestialBody body)
+        {
+
+        }
+
+        public static void Shine(string bodyName)
+        {
+
+        }
+
+        //Dractivate
+        public static void Dim(ICelestialBody body)
+        {
+
+        }
+
+        public static void Dim(string bodyName)
+        {
+
+        }
+
+        //Publish
+        public static async Task<OASISResult<IOAPP>> SeedAsync(string fullPathToOAPP, string launchTarget, string fullPathToPublishTo = "", bool registerOnSTARNET = true, bool dotnetPublish = true, bool generateOAPPSource = true, bool uploadOAPPSourceToSTARNET = true, bool makeOAPPSourcePublic = false, bool generateOAPPBinary = true, bool generateOAPPSelfContainedBinary = false, bool generateOAPPSelfContainedFullBinary = false, bool uploadOAPPToCloud = false, bool uploadOAPPSelfContainedToCloud = false, bool uploadOAPPSelfContainedFullToCloud = false, ProviderType providerType = ProviderType.Default, ProviderType oappBinaryProviderType = ProviderType.IPFSOASIS, ProviderType oappSelfContainedBinaryProviderType = ProviderType.None, ProviderType oappSelfContainedFullBinaryProviderType = ProviderType.None)
+        {
+            return await STARAPI.OAPPs.PublishOAPPAsync(BeamedInAvatar.AvatarId, fullPathToOAPP, launchTarget, fullPathToPublishTo, false, registerOnSTARNET, dotnetPublish, generateOAPPSource, uploadOAPPSourceToSTARNET, makeOAPPSourcePublic, generateOAPPBinary, generateOAPPSelfContainedBinary, generateOAPPSelfContainedFullBinary, uploadOAPPToCloud, uploadOAPPSelfContainedToCloud, uploadOAPPSelfContainedFullToCloud, providerType, oappBinaryProviderType, oappSelfContainedBinaryProviderType, oappSelfContainedFullBinaryProviderType);
+        }
+
+        public static OASISResult<IOAPP> Seed(string fullPathToOAPP, string launchTarget, string fullPathToPublishTo = "", bool registerOnSTARNET = true, bool dotnetPublish = true, bool generateOAPPSource = true, bool uploadOAPPSourceToSTARNET = true, bool makeOAPPSourcePublic = false, bool generateOAPPBinary = true, bool generateOAPPSelfContainedBinary = false, bool generateOAPPSelfContainedFullBinary = false, bool uploadOAPPToCloud = false, bool uploadOAPPSelfContainedToCloud = false, bool uploadOAPPSelfContainedFullToCloud = false, ProviderType providerType = ProviderType.Default, ProviderType oappBinaryProviderType = ProviderType.IPFSOASIS, ProviderType oappSelfContainedBinaryProviderType = ProviderType.None, ProviderType oappSelfContainedFullBinaryProviderType = ProviderType.None)
+        {
+            return STARAPI.OAPPs.PublishOAPP(BeamedInAvatar.AvatarId, fullPathToOAPP, launchTarget, fullPathToPublishTo, false, registerOnSTARNET, dotnetPublish, generateOAPPSource, uploadOAPPSourceToSTARNET, makeOAPPSourcePublic, generateOAPPBinary, generateOAPPSelfContainedBinary, generateOAPPSelfContainedFullBinary, uploadOAPPToCloud, uploadOAPPSelfContainedToCloud, uploadOAPPSelfContainedFullToCloud, providerType, oappBinaryProviderType, oappSelfContainedBinaryProviderType, oappSelfContainedFullBinaryProviderType);
+        }
+
+        public static async Task<OASISResult<OAPP>> UnSeedAsync(Guid OAPPId, int version = 0, ProviderType providerType = ProviderType.Default)
+        {
+            return await STARAPI.OAPPs.UnpublishAsync(BeamedInAvatar.Id, OAPPId, version, providerType);
+        }
+
+        public static OASISResult<OAPP> UnSeed(Guid OAPPId, int version = 0, ProviderType providerType = ProviderType.Default)
+        {
+            return STARAPI.OAPPs.Unpublish(BeamedInAvatar.Id, OAPPId, version, providerType);
+        }
+
+        // Run Tests
+        public static void Twinkle(ICelestialBody body)
+        {
+
+        }
+
+        public static void Twinkle(string bodyName)
+        {
+
+        }
+
+        // Delete Planet (OApp)
+        public static void Dust(ICelestialBody body)
+        {
+
+        }
+
+        // Delete Planet (OApp)
+        public static void Dust(string bodyName)
+        {
+
+        }
+
+        
+        public static void Evolve(ICelestialBody body)
+        {
+
+        }
+
+        public static void Evolve(string bodyName)
+        {
+
+        }
+
+        public static void Mutate(ICelestialBody body)
+        {
+
+        }
+
+        public static void Mutate(string bodyName)
+        {
+
+        }
+
+        // Highlight the Planet (OApp) in the OApp Store (StarNET)
+        public static void Radiate(ICelestialBody body)
+        {
+
+        }
+
+        public static void Radiate(string bodyName)
+        {
+
+        }
+
+        // Show how much light the planet (OApp) is emitting into the solar system (StarNET/HoloNET)
+        public static void Emit(ICelestialBody body)
+        {
+
+        }
+
+        public static void Emit(string bodyName)
+        {
+
+        }
+
+        // Show stats of the Planet (OApp)
+        public static void Reflect(ICelestialBody body)
+        {
+
+        }
+
+        public static void Reflect(string bodyName)
+        {
+
+        }
+
+        // Send/Receive Love
+        public static void Love(ICelestialBody body)
+        {
+
+        }
+
+        public static void Love(string body)
+        {
+
+        }
+
+        // Show network stats/management/settings
+        public static void Burst(ICelestialBody body)
+        {
+
+        }
+
+        public static void Burst(string body)
+        {
+
+        }
+
+        // ????
+        public static void Pulse(ICelestialBody body)
+        {
+
+        }
+
+        public static void Pulse(string body)
+        {
+
+        }
+
+        // Reserved For Future Use...
+        public static void Super(ICelestialBody body)
+        {
+
+        }
+
+        public static void Super(string planetName)
+        {
+
+        }
+    }
+}
