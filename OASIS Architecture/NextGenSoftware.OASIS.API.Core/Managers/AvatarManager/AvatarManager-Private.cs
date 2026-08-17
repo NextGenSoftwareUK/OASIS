955888b2208e4eae93c7dbfd2044166c97f6df14
David Ellams <davidellams@hotmail.com>
2026-08-16 22:31:54 -0500

    fix: add missing NextGenSoftware.Logging using in AvatarManager-Private.cs

--- OASIS Architecture/NextGenSoftware.OASIS.API.Core/Managers/AvatarManager/AvatarManager-Private.cs
+++ OASIS Architecture/NextGenSoftware.OASIS.API.Core/Managers/AvatarManager/AvatarManager-Private.cs
@@ -5,6 +5,7 @@ using BC = BCrypt.Net.BCrypt;
 using NextGenSoftware.OASIS.API.Core.Enums;
 using NextGenSoftware.OASIS.API.Core.Helpers;
 using NextGenSoftware.Utilities;
+using NextGenSoftware.Logging;
 using NextGenSoftware.OASIS.API.Core.Interfaces;
 using NextGenSoftware.OASIS.API.Core.Holons;
 using NextGenSoftware.OASIS.API.Core.Objects;
