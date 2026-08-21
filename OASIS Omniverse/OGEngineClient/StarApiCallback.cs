9c5de579889b018d4397bfb0e4d8fd933a8265e6
David Ellams <davidellams@hotmail.com>
2026-08-21 17:56:04 -0500

    refactor: replace OGEngineClient and ONODE Manager/Client/Service with private submodules

--- OASIS Omniverse/OGEngineClient/StarApiCallback.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace NextGenSoftware.OASIS.STARAPI.Client;
-
-public delegate void StarApiCallback(StarApiResultCode result, object? userData);
--- OASIS Omniverse/OGEngineClient/StarSyncCallbacks.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-namespace NextGenSoftware.OASIS.STARAPI.Client;
-
-[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-internal delegate void StarSyncDoneCallback(IntPtr userData);
-
-[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-internal delegate void StarSyncAddItemLogCallback(IntPtr itemName, int success, IntPtr errorMessage, IntPtr userData);
