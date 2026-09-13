5373c8cbf4be09eca0ac8d1284ce32d78df36fa2
David Ellams <davidellams@hotmail.com>
2026-09-13 01:01:07 -0500

    fix: resolve all build errors in 11 new OASIS providers
    - FastlyOASIS: fixed SaveHolons sync wrapper (missing curentChildDepth
      arg), fixed DeleteHolon(Guid) and DeleteHolon(string) calling
      DeleteHolonAsync with extra bool arg that base doesn't accept
    - DenoDeployOASIS: full rewrite with correct abstract method signatures,
      complete sync wrapper block, all missing async stubs
    - AxelarOASIS/WormholeOASIS: renamed LoadAvatarAsync->LoadAvatarByUsername
      Async, fixed ProviderCategory.Blockchain enum value, added all
      IOASISNETProvider stubs (GetAvatarsNearMe/GetHolonsNearMe), added all
      IOASISBlockchainStorageProvider stubs (13 method pairs)
    - CeramicOASIS/CivicOASIS/ReclaimProtocolOASIS: renamed LoadAvatarAsync,
      fixed avatarUsername body reference, added complete sync wrapper and
      async stub blocks
    - GOMapOASIS: added using Contracts.Enums to resolve CS0738 MapProviderType
      type-mismatch on interface implementation
    - IOASISMapProvider/IMapManager/MapManager: added using Contracts.Enums to
      resolve MapProviderType ambiguity in full-solution build
    
    Full solution (The OASIS - NoTests.sln) now builds: 0 errors.

--- OASIS Architecture/NextGenSoftware.OASIS.API.Managers/Interfaces/IMapManager.cs
+++ OASIS Architecture/NextGenSoftware.OASIS.API.Managers/Interfaces/IMapManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Drawing;
 using NextGenSoftware.OASIS.API.Contracts.Interfaces;
+using NextGenSoftware.OASIS.API.Contracts.Enums;
 
 namespace NextGenSoftware.OASIS.API.Managers.Interfaces
 {
