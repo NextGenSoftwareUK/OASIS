59f1927ca83ea322dab6c33a8cc42bb006fe1070
David Ellams <davidellams@hotmail.com>
2026-09-25 16:31:58 -0500

    Declare Mongo hosted synchronization contracts

---
+++ Providers/Storage/NextGenSoftware.OASIS.API.Providers.MongoOASIS.IntegrationTests/HostedMongoProviderContractTests.cs
@@ -0,0 +1,23 @@
+using NextGenSoftware.OASIS.API.Core.Managers.OASISHyperDrive.Synchronization;
+using Xunit;
+using MongoProvider = NextGenSoftware.OASIS.API.Providers.MongoDBOASIS.MongoDBOASIS;
+
+namespace NextGenSoftware.OASIS.API.Providers.MongoOASIS.IntegrationTests;
+
+public sealed class HostedMongoProviderContractTests
+{
+    [Fact]
+    public void MongoProvider_AdvertisesEveryHostedSynchronizationContract()
+    {
+        var providerType = typeof(MongoProvider);
+
+        Assert.True(typeof(IHostedHyperDriveSyncStore).IsAssignableFrom(providerType));
+        Assert.True(typeof(IHostedHyperDrivePeerBindingStore).IsAssignableFrom(providerType));
+        Assert.True(typeof(IHostedHyperDriveFanOutStore).IsAssignableFrom(providerType));
+        Assert.True(typeof(IHostedHyperDriveMaintenanceStore).IsAssignableFrom(providerType));
+        Assert.True(typeof(IHostedHyperDriveCommandStore).IsAssignableFrom(providerType));
+        Assert.True(typeof(IHostedHyperDriveDomainMutationStore).IsAssignableFrom(providerType));
+        Assert.True(typeof(IHostedHyperDriveDomainChangeCaptureStore).IsAssignableFrom(providerType));
+        Assert.True(typeof(IHostedHyperDriveDomainBackfillStore).IsAssignableFrom(providerType));
+    }
+}
